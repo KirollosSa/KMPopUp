68467fd6fbcf647c9bec38548a63d040a24d5585
apple <apple@apples-MacBook-Air.local>
2018-01-22 17:20:22 +0200

    Create Project Via pod creata lib.

---
+++ Example/Pods/Target Support Files/KMPopUp/KMPopUp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KMPopUp : NSObject
+@end
+@implementation PodsDummy_KMPopUp
+@end
---
+++ Example/Pods/Target Support Files/Pods-KMPopUp_Example/Pods-KMPopUp_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KMPopUp_Example : NSObject
+@end
+@implementation PodsDummy_Pods_KMPopUp_Example
+@end
