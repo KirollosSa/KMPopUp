1272a0b72f61df153b79c9b87829a30a1cee65d2
apple <apple@apples-macbook-air.local>
2018-01-24 11:02:34 +0200

    version 1.1.3

--- Classes/KMPoUp.swift
+++ Classes/KMPoUp.swift
@@ -10,10 +10,22 @@ import Foundation
 
 public class KMPoUp {
     
-    public static func DoneAction(controller : UIViewController , message: String = "",image : UIImage ,duration: Double = 1.0) {
+    public static func ShowMessageWithDuration(controller : UIViewController , message: String = "",image : UIImage ,duration: Double = 1.0) {
         let popUpVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "KMPoUpViewController") as! KMPoUpViewController
         popUpVC.message = message
         popUpVC.duration = duration
+        popUpVC.messageImage = image
+        controller.addChildViewController(popUpVC)
+        popUpVC.view.frame = controller.view.frame
+        controller.view.addSubview(popUpVC.view)
+        popUpVC.didMove(toParentViewController: controller)
+        
+    }
+    
+    public static func ShowMessage(controller : UIViewController , message: String = "",image : UIImage ) {
+        let popUpVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "KMPoUpViewController") as! KMPoUpViewController
+        popUpVC.message = message
+        popUpVC.duration = 0.0
         popUpVC.messageImage = image
         controller.addChildViewController(popUpVC)
         popUpVC.view.frame = controller.view.frame
--- Example/KMPopUp/ViewController.swift
+++ Example/KMPopUp/ViewController.swift
@@ -7,20 +7,31 @@
 //
 
 import UIKit
+import KMPopUp
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func duration(_ sender: Any) {
+        KMPoUp.ShowMessageWithDuration(controller: self, message: "Test", image: #imageLiteral(resourceName: "33"), duration: 1.0)
+    }
+    
+    
+    @IBAction func noDuration(_ sender: Any) {
+        KMPoUp.ShowMessage(controller: self, message: "test", image: #imageLiteral(resourceName: "33"))
+    }
+    @IBAction func Image(_ sender: Any) {
+        KMPoUp.ShowMessage(controller: self, message: "", image: #imageLiteral(resourceName: "33"))
+    }
 }
 
